e979120bad7bd86cbf5d2a47f9fdbff0cbc422a4
Matthew Garrettt <mdgarrett1984@gmail.com>
2016-08-11 16:37:51 -0500

    MODAAL

--- script.js
+++ script.js
@@ -19,68 +19,22 @@ $(function(){
 });
 
 
+// VIDEO GALLERY
 
 
-// MOTION GALLERY
+
+$('.gallery').modaal({
+  type: 'video'
+
+});
 
 
-   $('.gallery').each(function() { // the containers for all your galleries
-    $(this).find('a').magnificPopup({
-        type: 'image',
-        gallery: {
-          enabled:true
-        }
-    });
 
-  });
+// VIDEO GALLERY END
 
 
 
 
-   // STILL IMAGES
-
-  $('#FILM').magnificPopup({
-    items: [
-      {
-        src: 'img/66.jpg'
-
-      },
-
-      {
-        src: 'img/2.jpg'
-      },
-      {
-        src: 'img/120.jpg'
-      },
-
-      
-    ],
-    gallery: {
-      enabled: true
-    },
-    type: 'image' // this is default type
-}); 
-
-$('#DIGITAL').magnificPopup({
-    items: [
-      {
-        src: 'img/4.jpg'
-      },
-
-      {
-        src: 'img/111.jpg'
-      },
-      {
-        src: 'img/03.jpg'
-      },
-
-      
-    ],
-    gallery: {
-      enabled: true
-    },
-    type: 'image' // this is default type
-}); 
 
 
 
