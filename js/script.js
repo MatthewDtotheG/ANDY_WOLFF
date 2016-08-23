a3d7b654b26cb1efd9a5427096c58e2470a4c94c
Matthew Garrettt <mdgarrett1984@gmail.com>
2016-08-23 11:13:55 -0500

    Developing Mobile

--- js/script.js
+++ js/script.js
@@ -1,9 +1,7 @@
-$(document).ready(function() {
+
 //ANCHOR TAGS
 
 $(function(){
-
-
     $('a[href^="#"]').on('click',function (e) {
       e.preventDefault();
 
@@ -19,6 +17,11 @@ $(function(){
 });
 
 
+//ANCHOR TAGS END
+
+
+
+
 // VIDEO GALLERY
 
 $("#lightgallery").lightGallery({
@@ -27,6 +30,9 @@ $("#lightgallery").lightGallery({
 }); 
 
 // VIDEO GALLERY END
+
+
+
 
 
 
@@ -60,18 +66,17 @@ $('#FILM').on('click', function() {
 $('#DIGITAL').on('click', function() {
  
     $(this).lightGallery({
+        
         download: false,
         hideBarsDelay: 3000,
         enableTouch: true,
         dynamic: true,
         dynamicEl: [{
-            "src": 'img/2A.jpg',
-        }, {
-            'src': 'img/66.jpg',
+            "src": 'img/09.jpg',
         }, 
             {
             'src': 'img/111.jpg',
-        },
+            },
         {
             'src': 'img/44.jpg',
         }]
@@ -102,10 +107,38 @@ $('#RESUME').on('click', function() {
 
 
 
-var Froogaloop=function(){function e(a){return new e.fn.init(a)}function g(a,c,b){if(!b.contentWindow.postMessage)return!1;a=JSON.stringify({method:a,value:c});b.contentWindow.postMessage(a,h)}function l(a){var c,b;try{c=JSON.parse(a.data),b=c.event||c.method}catch(e){}"ready"!=b||k||(k=!0);if(!/^https?:\/\/player.vimeo.com/.test(a.origin))return!1;"*"===h&&(h=a.origin);a=c.value;var m=c.data,f=""===f?null:c.player_id;c=f?d[f][b]:d[b];b=[];if(!c)return!1;void 0!==a&&b.push(a);m&&b.push(m);f&&b.push(f);
-return 0<b.length?c.apply(null,b):c.call()}function n(a,c,b){b?(d[b]||(d[b]={}),d[b][a]=c):d[a]=c}var d={},k=!1,h="*";e.fn=e.prototype={element:null,init:function(a){"string"===typeof a&&(a=document.getElementById(a));this.element=a;return this},api:function(a,c){if(!this.element||!a)return!1;var b=this.element,d=""!==b.id?b.id:null,e=c&&c.constructor&&c.call&&c.apply?null:c,f=c&&c.constructor&&c.call&&c.apply?c:null;f&&n(a,f,d);g(a,e,b);return this},addEvent:function(a,c){if(!this.element)return!1;
-var b=this.element,d=""!==b.id?b.id:null;n(a,c,d);"ready"!=a?g("addEventListener",a,b):"ready"==a&&k&&c.call(null,d);return this},removeEvent:function(a){if(!this.element)return!1;var c=this.element,b=""!==c.id?c.id:null;a:{if(b&&d[b]){if(!d[b][a]){b=!1;break a}d[b][a]=null}else{if(!d[a]){b=!1;break a}d[a]=null}b=!0}"ready"!=a&&b&&g("removeEventListener",a,c)}};e.fn.init.prototype=e.fn;window.addEventListener?window.addEventListener("message",l,!1):window.attachEvent("onmessage",l);return window.Froogaloop=
-window.$f=e}();
+
+
+// COLLAPSIBLE NAV MENU
+
+
+$(document).ready(function(){
+  $('#nav-icon3').click(function(){
+    $(this).toggleClass('open');
+  });
+});
+
+
+
+
+$('.toggle-nav').click(function() {
+    if ($('body').hasClass('show-nav')) {
+        $('body').removeClass('show-nav').addClass('hide-nav');
+
+        setTimeout(function() {
+            $('body').removeClass('hide-nav');
+        }, 500);
+
+    } else {
+        $('body').removeClass('hide-nav').addClass('show-nav');
+    }
+
+    return false;
+});
+
+
+// COLLAPSIBLE NAV MENU END
+
 
 
 
@@ -125,5 +158,4 @@ $('.NAV').on('sticky-end', function () {
  $('.NAV').hide();
 });
 
-
-});
+// STICKY HEADER END
