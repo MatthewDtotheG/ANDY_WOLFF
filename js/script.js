e5cedf2e6c93a20f8a7d1cdb621defcaba8e994a
Chris Gerringer <paperbeatsscissors@gmail.com>
2016-08-24 22:19:56 -0700

    Added a mobile menu bitches

--- js/script.js
+++ js/script.js
@@ -122,18 +122,7 @@ $(document).ready(function(){
 
 
 $('.toggle-nav').click(function() {
-    if ($('body').hasClass('show-nav')) {
-        $('body').removeClass('show-nav').addClass('hide-nav');
-
-        setTimeout(function() {
-            $('body').removeClass('hide-nav');
-        }, 500);
-
-    } else {
-        $('body').removeClass('hide-nav').addClass('show-nav');
-    }
-
-    return false;
+    $('#NAVMENU').slideToggle(250);
 });
 
 
